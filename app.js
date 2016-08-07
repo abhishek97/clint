831ca43a908a0e18881ec88edf268e716359fe33
piyush <kakkarpiyush96@gmail.com>
2016-08-07 13:35:57 +0530

    added some shuffling

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var path = require('path');
+
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
 var MongoClient = require('mongodb').MongoClient;
 
 
